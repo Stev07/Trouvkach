3cfd8f4a27ab316ebbac57af62adf47908beecb3
Steve Dossin <steve.dossin@gmail.com>
2019-02-23 22:41:14 +0100

    set initial zoom to users current position

--- src/client/components/hello.js
+++ src/client/components/hello.js
@@ -7,45 +7,38 @@
  */
 
 import * as React from "react";
-import Geolocation from "react-geolocation";
-
+// import Geolocation from "react-geolocation";
+// "pk.eyJ1Ijoic3RldjA3IiwiYSI6ImNqc2RkZm13ZTA2Y3o0OW1kb3N2eGo4bGoifQ.xzYNRvfOYN7zcoU8bnabJA"
 export default class HelloWorld extends React.Component {
     render() {
         const mapStyle = {width: "640px", height: "480px"};
 
-        window.onload = function() {
-            console.log("Page chargée!");
-            let mymap = L.map("mapid").setView([51.505, -0.09], 13);
+        window.onload = () => {
+            navigator.geolocation.getCurrentPosition(function(location) {
+                let latlng = new L.LatLng(
+                        location.coords.latitude,
+                        location.coords.longitude,
+                    ),
+                    mymap = L.map("mapid").setView(latlng, 10),
+                    marker = L.marker(latlng).addTo(mymap);
 
-            L.tileLayer(
-                "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
-                {
-                    attribution:
-                        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    maxZoom: 18,
-                    id: "mapbox.streets",
-                    accessToken:
-                        "pk.eyJ1Ijoic3RldjA3IiwiYSI6ImNqc2RkZm13ZTA2Y3o0OW1kb3N2eGo4bGoifQ.xzYNRvfOYN7zcoU8bnabJA",
-                },
-            ).addTo(mymap);
+                L.tileLayer(
+                    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+                    {
+                        attribution:
+                            'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
+                        maxZoom: 18,
+                        id: "mapbox.streets",
+                        accessToken:
+                            "pk.eyJ1Ijoic3RldjA3IiwiYSI6ImNqc2RkZm13ZTA2Y3o0OW1kb3N2eGo4bGoifQ.xzYNRvfOYN7zcoU8bnabJA",
+                    },
+                ).addTo(mymap);
+            });
         };
+
         return (
             <div>
                 <h1>{"Hello, bro!"}</h1>
-                <Geolocation
-                    render={({
-                        position: {coords: {latitude, longitude} = {}} = {},
-                        error,
-                    }) => (
-                        <div>
-                            {error && <div>{error.message}</div>}
-                            <pre>
-                                {"latitude:"} {latitude}
-                                {"longitude:"} {longitude}
-                            </pre>
-                        </div>
-                    )}
-                />
                 <div id="mapid" style={mapStyle} />
             </div>
         );
